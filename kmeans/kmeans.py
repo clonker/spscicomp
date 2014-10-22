9bf54ff3e7308d4cfcf0933508ddea9f82ca9fa2
Moritz Hoffmann <clonker@gmail.com>
2014-10-22 23:49:01 +0200

    updated test file to pass setup, added default metric for kmeans abstract class

--- kmeans/kmeans.py
+++ kmeans/kmeans.py
@@ -1,10 +1,11 @@
 from abc import ABCMeta, abstractmethod
+from kmeans_metric import EuclideanMetric
 
 class Kmeans:
     """ abstract metric """
     __metaclass__ = ABCMeta
 
-    def __init__(self, metric):
+    def __init__(self, metric=EuclideanMetric()):
         self._metric = metric
 
     @abstractmethod
--- kmeans/test_kmeans.py
+++ kmeans/test_kmeans.py
@@ -2,15 +2,16 @@ import unittest
 import numpy as np
 from kmeans_metric import EuclideanMetric
 
+
 class TestKmeansMetric(unittest.TestCase):
     def setUp(self):
-        self.maybe = ''
+        pass
 
     def test_euclidean_metric(self):
         metric = EuclideanMetric()
-        self.assertEqual(metric.dist(np.array([0,0]), np.array([0,0])), 0, 'for points (0,0) and (0,0): Expected distance is zero!')
-        self.assertEqual(metric.dist(np.array([0,0]), np.array([3,4])), 5, 'for euclidean metric: d((0,0),(3,4))=5.')
-        self.assertEqual(metric.dist(np.array([3,4]), np.array([0,0])), 5, 'any metric should be symmetric.')
+        self.assertEqual(metric.dist(np.array([0, 0]), np.array([0, 0])), 0, 'for points (0,0) and (0,0): Expected distance is zero!')
+        self.assertEqual(metric.dist(np.array([0, 0]), np.array([3, 4])), 5, 'for euclidean metric: d((0,0),(3,4))=5.')
+        self.assertEqual(metric.dist(np.array([3, 4]), np.array([0, 0])), 5, 'any metric should be symmetric.')
 
 
 """

f0e8a5cfe2e1472ad8bc33693556d2876428d7b1
Moritz Hoffmann <clonker@gmail.com>
2014-10-24 10:04:23 +0200

    ported minibatchkmeans

--- kmeans/kmeans.py
+++ kmeans/kmeans.py
@@ -1,5 +1,8 @@
 from abc import ABCMeta, abstractmethod
+from numpy import random as rand
+import numpy as np
 from kmeans_metric import EuclideanMetric
+
 
 class Kmeans:
     """ abstract metric """
@@ -21,3 +24,60 @@ class DefaultKmeans(Kmeans):
 class WeakKmeans(Kmeans):
     def calculate_centers(self, k, data):
         return ''
+
+
+class MiniBatchKMeans(Kmeans):
+    def __init__(self, metric=EuclideanMetric(), batch_size=20, max_steps=100):
+        super(MiniBatchKMeans, self).__init__(metric)
+        self._batch_size = batch_size
+        self._max_steps = max_steps
+
+    def miniBatchKmeansIterate(self, data, centers, centers_counter):
+        mini_batch_centers = [list([]) for _ in xrange(self._batch_size)]
+
+        for i in xrange(0, self._batch_size):
+            j = rand.random.randint(0, 1000)
+            closest_center = self.ClosestCenter(data[j], centers)
+            mini_batch_centers[closest_center].append(j)
+
+        for i, mini_batch_center in enumerate(mini_batch_centers):
+            for j in mini_batch_center:
+                centers_counter[i] += 1
+                eta = 1.0 / centers_counter[i]
+                centers[i] = centers[i] * (1.0 - eta) + data[j] * eta
+
+        return centers, centers_counter
+
+    def calculate_centers(self, k, data):
+        centers = None
+        centers_counter = np.zeros(k)
+        i = 1
+
+        while True:  # not np.array_equal(centers, old_centers):
+            centers, centers_counter = self.miniBatchKmeansIterate(data, centers, centers_counter)
+
+            i += 1
+            if i > self._max_steps:
+                break
+
+        print i
+
+        return centers
+
+    def ClosestCenter(self, p, centers):
+        min_dist = float('inf')
+        closest_center = 0
+        for i, center in enumerate(centers):
+            dist = self._metric.dist(p, center)
+            if dist < min_dist:
+                min_dist = dist
+                closest_center = i
+
+        return int(closest_center)
+
+    def zeroCenters(self, k, d):
+        centers = []
+        for i in xrange(0, k):
+            centers.append(np.zeros(d))
+
+        return centers
--- kmeans/test_kmeans.py
+++ kmeans/test_kmeans.py
@@ -13,6 +13,10 @@ class TestKmeansMetric(unittest.TestCase):
         self.assertEqual(metric.dist(np.array([0, 0]), np.array([3, 4])), 5, 'for euclidean metric: d((0,0),(3,4))=5.')
         self.assertEqual(metric.dist(np.array([3, 4]), np.array([0, 0])), 5, 'any metric should be symmetric.')
 
+    def test_mini_batch_kmeans(self):
+        # TODO
+        print ''
+
 
 """
     main

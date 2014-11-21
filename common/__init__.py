e8c298bec521a060349ae26fa8ffcb34a24b0895
florianlitzinger <florianlitzinger@users.noreply.github.com>
2014-11-21 15:25:57 +0100

    updated documentation.

--- common/__init__.py
+++ common/__init__.py
@@ -1,1 +1,17 @@
-"""This package contains classes which may be of use in all parts of the project."""
+"""
+This package contains classes which may be of use in all parts of the project.
+
+The DataImporter classes provide methods to do sequential reads of a specific size from a specified data source.
+The basic usage is as follows:
+-------------------------------------
+while True:
+    data = importer.get_data(size)
+    if len(data) is 0:
+        break
+    else:
+        # Do something
+    if not importer.has_more_data():
+        break
+importer.rewind()
+-------------------------------------
+"""

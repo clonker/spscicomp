4b59f4f284a302a121f747c5b33a6d22abd59084
Maikel Nadolski <maikel.nadolski@fu-berlin.de>
2015-01-06 10:04:08 +0100

    delete old files

--- hmm/extension/setup.py
+++
@@ -1,8 +0,0 @@
-from distutils.core import setup, Extension
-import numpy
-
-# define the extension module
-hmm_ext = Extension('hmm_ext', sources=['hmm_ext.c', 'hmm.c', 'hmm32.c'], include_dirs=[numpy.get_include()])
-
-# run the setup
-setup(ext_modules=[hmm_ext])

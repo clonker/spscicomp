1902856acf51df44b6498351080eb89572825b32
chrisfroe <christoph.froehner@fu-berlin.de>
2015-01-08 19:04:31 +0100

    Reverted change on genseq.py

--- hmm/bin/genseq.py
+++ hmm/bin/genseq.py
@@ -1,19 +1,8 @@
 #!/usr/bin/python
-import numpy as np
 import hmm.utility
-
-A = np.array(	[[0.3, 0.7, 0.0],
-				 [0.1, 0.2, 0.7],
-				 [0.0, 0.4, 0.6]])
-
-B = np.array(	[[0.3, 0.3, 0.3, 0.1],
-				 [0.1, 0.1, 0.1, 0.7],
-				 [0.2, 0.2, 0.2, 0.4]])
-
-pi = np.array(	[[0.2],
-				 [0.4],
-				 [0.4]])
-
-obs = hmm.utility.random_sequence(A, B, pi, 1000, kernel=hmm.kernel.python)
-
-print obs
+import hmm.models
+import sys
+A, B, pi = hmm.models.get_models()[sys.argv[1]]
+seq = hmm.utility.generate_sequence(A, B, pi, int(sys.argv[2]))
+for ob in seq:
+	print ob

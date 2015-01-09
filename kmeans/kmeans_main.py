684898569cd2824fcdf005a46670070fc3ad337d
Moritz Hoffmann <clonker@gmail.com>
2015-01-09 10:12:50 +0100

    Updated the main method.

--- kmeans/kmeans_main.py
+++ kmeans/kmeans_main.py
@@ -7,25 +7,26 @@ def kmeans(k, importer=None):
         try:
             from cuda.cuda_kmeans import CUDAKmeans
 
-            kmeans_implementation = CUDAKmeans(k, importer=importer)
+            kmeans_implementation = CUDAKmeans(importer=importer)
         except:
             try:
                 from opencl.opencl_kmeans import OpenCLKmeans
 
-                kmeans_implementation = OpenCLKmeans(k, importer=importer)
+                kmeans_implementation = OpenCLKmeans(importer=importer)
             except:
                 try:
                     from extension.c_kmeans import CKmeans
 
-                    kmeans_implementation = CKmeans(k, importer=importer)
+                    kmeans_implementation = CKmeans(importer=importer)
                 except:
                     print 'Failed to initialize any of the optimized kmeans implementations, using default one'
 
         if not kmeans_implementation:
-            kmeans_implementation = DefaultKmeans(k, importer=importer)
+            kmeans_implementation = DefaultKmeans(importer=importer)
     else:
         print 'Needs data importer!'
     print 'implementation chosen = ' + str(type(kmeans_implementation))
     if kmeans_implementation and importer:
-        return kmeans_implementation.calculate_centers(k)
+        c, _ = kmeans_implementation.calculate_centers(k, return_centers=True)
+        return c
     return None

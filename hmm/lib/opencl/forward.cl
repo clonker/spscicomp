0fb2a62f600b5684a0c5648fd01d3b610529099b
Maikel Nadolski <maikel.nadolski@fu-berlin.de>
2015-01-13 17:45:28 +0100

    ongoing work

--- hmm/lib/opencl/forward.cl
+++ hmm/lib/opencl/forward.cl
@@ -39,6 +39,14 @@ for (int _i = 0; _i < N; _i++) \
 
 #endif /* __DIMS__ */
 
+#define MATRIX_MULTIPLY_NS(AB, A, B) \
+for (int _i = 0; _i < N; _i++) \
+   for (int _j = 0; _j < N; _j++) { \
+      AB[_i][_j] = 0.0f; \
+      for (int _k = 0; _k < N; _k++) \
+         AB[_i][_j] += A[_i][_k]*B[_k][_j]; \
+   }
+
 #define MATRIX_MULTIPLY(AB, A, B) \
 ${precision} __sum = 0.0f; \
 for (int _i = 0; _i < N; _i++) \
@@ -56,6 +64,14 @@ for (int _i = 0; _i < N; _i++) \
 for (int _i = 0; _i < N; _i++) \
    for (int _j = 0; _j < N; _j++) \
       dest[_i][_j] = src[_i][_j];
+
+#define COPY_ID(dest) \
+for (int _i = 0; _i < N; _i++) \
+   for (int _j = 0; _j < N; _j++) \
+      if (_i == _j) \
+         dest[_i][_j] = 1.0f; \
+      else \
+         dest[_i][_j] = 0.0f
 
 
 /* Create the vector of matrices which we want to do the cumulative sum
@@ -106,18 +122,15 @@ void sum_sequential(
       size_t end,
       int T)
 {
-   if (start < T) {
-      ${precision} C[N][N];
-      ${precision} L[N][N], R[N][N];
-      GET_DIM2(C, in, start);
-      for (size_t i = start+1; i < end && i < T; i++) {
-         GET_DIM2(R, in, i);
-         MATRIX_MULTIPLY(L, R, C);
-         COPY(C, L);
-      }
-      PUT_DIM2(C, out, out_id);
-   } else 
-      PUT_ID(out, out_id);
+   ${precision} C[N][N];
+   ${precision} L[N][N], R[N][N];
+   GET_DIM2(C, in, start);
+   for (size_t i = start+1; i < end; i++) {
+      GET_DIM2(R, in, i);
+      MATRIX_MULTIPLY(L, R, C);
+      COPY(C, L);
+   }
+   PUT_DIM2(C, out, out_id);
 }
 
 kernel void reduce(
@@ -127,34 +140,45 @@ kernel void reduce(
       constant int *transform,
       int T)
 {
-   size_t HALF_BLOCK = get_local_size(0);
-   size_t BLOCK_SIZE = HALF_BLOCK << 1;
-
-   size_t T_seq = T / (BLOCK_SIZE * get_num_groups(0)) + 1;
+   size_t NUM_BLOCK_THREADS = get_local_size(0);
+   size_t BLOCK_SIZE = NUM_BLOCK_THREADS << 1;
+   size_t global_id = get_global_id(0);
+   size_t TOTAL_SIZE = BLOCK_SIZE*get_num_groups(0);
+   size_t num_corrections = (T / BLOCK_SIZE) % get_num_groups(0);
+   size_t sequential_len = T / TOTAL_SIZE;
+   size_t T_start;
+   if (get_group_id(0) >= num_corrections)
+      T_start = (sequential_len+1)*num_corrections*BLOCK_SIZE
+            + sequential_len*(2*global_id - num_corrections*BLOCK_SIZE);
+   else {
+      sequential_len = sequential_len + 1;
+      T_start = sequential_len*2*global_id;
+   }
 
    ${precision} C[N][N];
-   ${precision} L[N][N] = {0.0f}, R[N][N] = {0.0f};
+   ${precision} L[N][N];
+   ${precision} R[N][N];
 
    size_t local_id = get_local_id(0);
    size_t left  = transform[2*local_id];
    size_t right = transform[2*local_id+1];
-   size_t gid = get_global_id(0);
 
    sum_sequential(scratch, left,
-      in, 2*gid*T_seq, (2*gid+1)*T_seq, T);
+      in, T_start, T_start + sequential_len, T);
    sum_sequential(scratch, right,
-      in, (2*gid+1)*T_seq, (2*gid+2)*T_seq, T);
+      in, T_start + sequential_len, T_start + 2*sequential_len, T);
 
    barrier(CLK_LOCAL_MEM_FENCE);
 
-   size_t bi = local_id + HALF_BLOCK;
+   size_t bi = local_id + NUM_BLOCK_THREADS;
    GET_DIM2(C, scratch, bi);
-   for (size_t offset = HALF_BLOCK;
+   for (size_t offset = NUM_BLOCK_THREADS;
                offset > 0;
                offset >>= 1)
    {
       barrier(CLK_LOCAL_MEM_FENCE);
-      if (HALF_BLOCK <= local_id + offset) {
+
+      if (NUM_BLOCK_THREADS <= local_id + offset) {
          size_t ai = bi - offset;
          GET_DIM2(L, scratch, ai);
          MATRIX_MULTIPLY(R, C, L);
@@ -163,83 +187,150 @@ kernel void reduce(
       }
    }
 
-   if (local_id == HALF_BLOCK-1)
+   if (local_id == NUM_BLOCK_THREADS-1) 
       PUT_DIM2(C, out, get_group_id(0));
 }
 
-
+/*
+ * inplace toplevel scan with permutation
+ */
 kernel void scan(
       global ${precision} *in,
-      global ${precision} *out,
       local ${precision} *scratch,
       constant int *transform,
       int T)
 {
-   size_t HALF_BLOCK = get_local_size(0);
-   size_t BLOCK_SIZE = HALF_BLOCK << 1;
+   size_t NUM_BLOCK_THREADS = get_local_size(0);
+//   size_t BLOCK_SIZE = NUM_BLOCK_THREADS << 1;
 
    size_t local_id = get_local_id(0);
    size_t left  = transform[2*local_id];
    size_t right = transform[2*local_id+1];
    size_t global_id = get_global_id(0);
-   size_t group_root = global_id - local_id;
 
-   size_t T_segment = T / (get_num_groups(0)*BLOCK_SIZE);
-
-   local ${precision} last_element[N][N];
+   ${precision} last_element[N][N];
    ${precision} C[N][N];
    ${precision} L[N][N] = {0.0f}, R[N][N] = {0.0f};
 
-   while (group_root < T_segment) {
-
+   if (2*global_id < T) {
       GET_DIM2(C, in, 2*global_id);
       PUT_DIM2(C, scratch, left);
+   } else
+      PUT_ID(scratch, left);
+
+   if (2*global_id+1 < T) {
       GET_DIM2(C, in, 2*global_id+1);
       PUT_DIM2(C, scratch, right);
+   } else
+      PUT_ID(scratch, right);
 
+   barrier(CLK_LOCAL_MEM_FENCE);
+
+   size_t bi = local_id + NUM_BLOCK_THREADS;
+   GET_DIM2(C, scratch, bi);
+   for (size_t offset = NUM_BLOCK_THREADS;
+               offset > 0;
+               offset >>= 1)
+   {
       barrier(CLK_LOCAL_MEM_FENCE);
-      size_t bi = local_id + HALF_BLOCK;
-      GET_DIM2(C, scratch, bi);
-      for (size_t offset = HALF_BLOCK;
-                  offset > 0;
-                  offset >>= 1)
-      {
-         barrier(CLK_LOCAL_MEM_FENCE);
-         if (HALF_BLOCK <= local_id + offset) {
-            size_t ai = bi - offset;
-            GET_DIM2(L, scratch, ai);
-            MATRIX_MULTIPLY(R, C, L);
-            PUT_DIM2(R, scratch, bi);
-            COPY(C,R);
-         }  
-      }
-
-      if (local_id == HALF_BLOCK-1) {
-         COPY(last_element, C);
-         PUT_ID(*C, 0);
-      }
-
-      for (size_t offset = 1;
-                  offset <= HALF_BLOCK;
-                  offset <<= 1)
-      {
-         barrier(CLK_LOCAL_MEM_FENCE);
-         if (HALF_BLOCK <= local_id + offset) {
-            size_t ai = bi - offset;
-            GET_DIM2(L, scratch, ai);
-            MATRIX_MULTIPLY(R, L, C);
-            PUT_DIM2(C, scratch, ai);
-            COPY(C,R);
-         }
-      }
-
-      PUT_DIM2(C, in, 2*global_id);
-      if (local_id == HALF_BLOCK-1)
-         PUT_DIM2(last_element, in, 2*global_id+1);
-      else if (local_id != 0) {
-         GET_DIM2(C, scratch, local_id);
-         PUT_DIM2(C, in, 2*global_id-1);
-      }
-
+      if (NUM_BLOCK_THREADS <= local_id + offset) {
+         size_t ai = bi - offset;
+         GET_DIM2(L, scratch, ai);
+         MATRIX_MULTIPLY_NS(R, C, L);
+         PUT_DIM2(R, scratch, bi);
+         COPY(C,R);
+      }  
    }
+
+   if (local_id == NUM_BLOCK_THREADS-1) {
+      COPY(last_element, C);
+      COPY_ID(C);
+      PUT_DIM2(C, scratch, bi);
+   }
+
+   for (size_t offset = 1;
+               offset <= NUM_BLOCK_THREADS;
+               offset <<= 1)
+   {
+      barrier(CLK_LOCAL_MEM_FENCE);
+      if (NUM_BLOCK_THREADS <= local_id + offset) {
+         size_t ai = bi - offset;
+         GET_DIM2(L, scratch, ai);
+         GET_DIM2(C, scratch, bi);
+         PUT_DIM2(C, scratch, ai);
+         MATRIX_MULTIPLY_NS(R, L, C);
+         PUT_DIM2(R, scratch, bi);
+      }
+   }
+
+   if (2*global_id < T) {
+      GET_DIM2(C, scratch, right);
+      PUT_DIM2(C, in, 2*global_id);
+   } if (local_id == NUM_BLOCK_THREADS-1 && 2*global_id+1 < T)
+      PUT_DIM2(last_element, in, 2*global_id+1);
+   else if (local_id != 0 && 2*global_id-1 < T) {
+      GET_DIM2(C, scratch, left);
+      PUT_DIM2(C, in, 2*global_id-1);
+   }
+}
+
+/*
+ * inplace toplevel scan with permutation
+ */
+kernel void scan_bottom(
+      global ${precision} *in,
+      global ${precision} *toplevel,
+      local ${precision} *scratch,
+      constant int *transform,
+      global ${precision} *debug,
+      int T)
+{
+   size_t group_id = get_group_id(0);
+   size_t NUM_BLOCK_THREADS = get_local_size(0);
+   size_t BLOCK_SIZE = NUM_BLOCK_THREADS << 1;
+   size_t global_id = get_global_id(0);
+   size_t TOTAL_SIZE = BLOCK_SIZE*get_num_groups(0);
+   size_t num_corrections = (T / BLOCK_SIZE) % get_num_groups(0);
+   int sequential_len = T / TOTAL_SIZE;
+   int T_start;
+   if (get_group_id(0) >= num_corrections)
+      T_start = (sequential_len+1)*num_corrections*BLOCK_SIZE
+            + sequential_len*(2*global_id - num_corrections*BLOCK_SIZE);
+   else {
+      sequential_len = sequential_len + 1;
+      T_start = sequential_len*2*global_id;
+   }
+
+   ${precision} C_0[N][N] = { 0.0f };
+   ${precision} A[N][N] = { 0.0f };
+   ${precision} B[N][N] = { 0.0f };
+   GET_DIM2(C_0, in, T_start);
+   if (get_group_id(0) > 0) {
+//      GET_DIM2(A, toplevel, get_group_id(0)-1);
+//      GET_DIM2(C_0, in, T_start);
+//      MATRIX_MULTIPLY_NS(C_0A, C_0, A);
+      for (int i = 0; i < N; i++)
+         for (int j = 0; j < N; j++)
+            DIM3(toplevel, 0, i, j) = 0.0f;
+   }
+   B[0][0] = get_group_id(0);
+   B[0][1] = get_local_id(0);
+   B[0][2] = get_global_id(0);
+   B[1][0] = T_start;
+   B[1][1] = sequential_len;
+   B[1][2] = T;
+   PUT_DIM2(B, debug, 2*global_id);
+   PUT_DIM2(B, debug, 2*global_id+1);
+
+   // while (sequential_len > BLOCK_SIZE) {
+   //    scan(in + T_start, scratch, transform, BLOCK_SIZE);
+
+   //    sequential_len -= BLOCK_SIZE;
+   //    T_start += BLOCK_SIZE;
+   //    GET_DIM2(A, in, T_start-1);
+   //    GET_DIM2(C_0, in, T_start);
+   //    MATRIX_MULTIPLY_NS(C_0A, C_0, A);
+   //    PUT_DIM2(C_0A, in, T_start);
+   // }
+//   scan(in + T_start, scratch, transform, sequential_len);
 }
--- hmm/tests/test_lib_opencl.py
+++ hmm/tests/test_lib_opencl.py
@@ -8,7 +8,7 @@ import hmm.kernel.c as chmm
 import time as t
 
 platform = pyopencl.get_platforms()[0]
-device   = platform.get_devices()[0]
+device   = platform.get_devices()[1]
 context  = pyopencl.Context([device])
 queue    = pyopencl.CommandQueue(context)
 
@@ -54,9 +54,119 @@ class TestOpenCLMatchesPython(TestTemplateKernel):
     def setUp(self):
         self.SOURCE = 'lib/opencl/forward.cl'
 
-    def test_forward_reduce(self):
+    def test_forward_scan_bottom_level(self):
         A, B, pi = hmm.utility.get_models()['t2']
-        ob = numpy.loadtxt('data/hmm1.1000000.dat', numpy.int16) # numpy.array([1,0,1,0], numpy.int16)
+        ob = numpy.loadtxt('data/hmm1.10.dat', numpy.int16)
+        T = len(ob)
+        N,M = B.shape
+        kernel = self.get_kernel(N, M, T)
+        mf = pyopencl.mem_flags
+
+        # prepare buffers
+        bA = pyopencl.Buffer(context,
+            mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=A)
+        bB = pyopencl.Buffer(context,
+            mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=B)
+        bpi = pyopencl.Buffer(context,
+            mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=pi)
+        bob = pyopencl.Buffer(context,
+            mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=ob)
+        bC = pyopencl.Buffer(context, mf.READ_WRITE,
+            numpy.dtype('float32').itemsize * T*N*N)
+        balpha_T = pyopencl.Buffer(context, mf.READ_WRITE,
+            numpy.dtype('float32').itemsize * N*N)
+
+        kernel.build_matrices(queue,
+                (64*256,), (256,),
+                bC, bA, bB, bpi, bob, numpy.int32(T))
+
+        start = t.time()
+        _, alpha, _ = chmm.forward(A, B, pi, ob)
+        end = t.time()
+        c_time = end-start
+        print 'forward time in c: {t} seconds.'.format(t=c_time)
+
+        blockss    = [1*n for n in range(1,40)]
+        blocksizes = [2**n for n in range(9)]
+
+        minimum = (0, 0, 1.0)
+        print 'benchmarking hmm.lib.opencl.forward.scan_bottom_level'
+        for blocksize in blocksizes:
+            for blocks in blockss:
+                if blocks <= 2*blocksize and blocks*blocksize*2 <= T:
+                    TOTAL_UNITS = 2*blocksize*blocks
+                    BLOCK_UNITS = 2*blocksize
+                    print "time: {T}\nblocks: {b}\nblocksize: {bs}\n"\
+                          "TOTAL_UNITS: {tu}\nBLOCK_UNITS: {bu}".format(
+                        T=T, b=blocks, bs=blocksize, tu=TOTAL_UNITS, bu=BLOCK_UNITS)
+
+                    balpha = pyopencl.Buffer(context, mf.READ_WRITE,
+                        numpy.dtype('float32').itemsize * blocks*N*N)
+                    bdebug = pyopencl.Buffer(context, mf.READ_WRITE,
+                        numpy.dtype('float32').itemsize * TOTAL_UNITS*N*N)
+                    scratch = pyopencl.LocalMemory(
+                        numpy.dtype('float32').itemsize*BLOCK_UNITS*N*N)
+
+                    transform = numpy.array(
+                        calculate_index_table([0], numpy.log2(blocksize)+1),
+                        numpy.int32)
+                    btransform = pyopencl.Buffer(context,
+                        mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=transform)
+
+                    # pyopencl.enqueue_copy(queue, reduced, balpha)
+                    # print 'reduced:\n', reduced
+
+                    e = pyopencl.enqueue_barrier(queue)
+                    e.wait()
+                    start = t.time()
+                    kernel.reduce(queue,
+                            (blocks*blocksize,), (blocksize,),
+                            bC, balpha, scratch, btransform, numpy.int32(T))
+                    kernel.scan(queue,
+                            (blocksize,), (blocksize,),
+                            balpha, scratch, btransform, numpy.int32(blocks))
+                    e = pyopencl.enqueue_barrier(queue)
+                    e.wait()
+                    
+                    kernel.scan_bottom(queue,
+                            (blocks*blocksize,), (blocksize,),
+                            bC, balpha, scratch, btransform, bdebug, numpy.int32(T))
+                    e = pyopencl.enqueue_barrier(queue)
+                    e.wait()
+                    end = t.time()
+
+                    debug = numpy.zeros((TOTAL_UNITS, N, N), numpy.float32)
+                    pyopencl.enqueue_copy(queue, debug, bdebug)
+                    print 'debug:\n', debug
+
+                    C = numpy.zeros((blocks,N,N), numpy.float32)
+                    pyopencl.enqueue_copy(queue, C, balpha)
+                    print C
+
+                    # C_T = C[blocks-1]
+                    # T0 = (T/BLOCK_UNITS) * BLOCK_UNITS
+                    # for k in range(T%BLOCK_UNITS):
+                    #     C_T = C[T0+k].dot(C_T)
+                    #     C_T = C_T / numpy.sum(C_T)
+
+                    # alpha0 = pi * B[:, ob[0]]
+                    # alphaT = C_T.dot(alpha0)
+                    # alphaT = alphaT / numpy.sum(alphaT)
+                    # numpy.testing.assert_almost_equal(alphaT, alpha[T-1])
+                    # print 'scanned:\n', reduced
+
+                    if end-start <= minimum[2] and \
+                       blocks    >= minimum[1] and \
+                       blocksize >= minimum[0]:
+                       minimum = (blocksize, blocks, end-start)
+
+        print "minimum of {t} seconds achieved with block size {bs} and {b} groups".format(
+            b=minimum[1], bs=minimum[0], t=minimum[2])
+        print "speedup: {s}".format(s=c_time/(minimum[2]))
+
+    def test_forward_scan_top_level(self):
+        A, B, pi = hmm.utility.get_models()['t2']
+        ob = numpy.loadtxt('data/hmm1.100.dat', numpy.int16)
         T = len(ob)
         N,M = B.shape
         kernel = self.get_kernel(N, M, T)
@@ -76,25 +186,36 @@ class TestOpenCLMatchesPython(TestTemplateKernel):
         balpha_T = pyopencl.Buffer(context, mf.WRITE_ONLY,
             numpy.dtype('float32').itemsize * N*N)
 
+        kernel.build_matrices(queue,
+                (64*256,), (256,),
+                bC, bA, bB, bpi, bob, numpy.int32(T))
+
         start = t.time()
         _, alpha, _ = chmm.forward(A, B, pi, ob)
         end = t.time()
         c_time = end-start
         print 'forward time in c: {t} seconds.'.format(t=c_time)
-        T = numpy.int32(T)
 
-        blockss    = [14*n for n in range(1,40)]
+        blockss    = [7*n for n in range(1,40)]
         blocksizes = [2**n for n in range(9)]
 
         minimum = (0, 0, 1.0)
-        print 'benchmarking hmm.lib.opencl.forward.reduce'
+        print 'benchmarking hmm.lib.opencl.forward.scan_top_level'
         for blocksize in blocksizes:
             for blocks in blockss:
-                if blocks < 2*blocksize:
+                if blocks <= 2*blocksize and blocks*blocksize*2 <= T:
+                    TOTAL_UNITS = 2*blocksize*blocks
+                    BLOCK_UNITS = 2*blocksize
+                    # print "time: {T}\nblocks: {b}\nblocksize: {bs}\n"\
+                    #       "TOTAL_UNITS: {tu}\nBLOCK_UNITS: {bu}".format(
+                    #     T=T, b=blocks, bs=blocksize, tu=TOTAL_UNITS, bu=BLOCK_UNITS)
+
                     balpha = pyopencl.Buffer(context, mf.READ_WRITE,
                         numpy.dtype('float32').itemsize * blocks*N*N)
+                    # bdebug = pyopencl.Buffer(context, mf.WRITE_ONLY,
+                    #     numpy.dtype('float32').itemsize * TOTAL_UNITS*N*N)
                     scratch = pyopencl.LocalMemory(
-                        numpy.dtype('float32').itemsize*2*blocksize*N*N)
+                        numpy.dtype('float32').itemsize*BLOCK_UNITS*N*N)
 
                     transform = numpy.array(
                         calculate_index_table([0], numpy.log2(blocksize)+1),
@@ -102,29 +223,136 @@ class TestOpenCLMatchesPython(TestTemplateKernel):
                     btransform = pyopencl.Buffer(context,
                         mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=transform)
 
-                    # execute kernels
-                    kernel.build_matrices(queue,
-                                (64*256,), (256,),
-                                bC, bA, bB, bpi, bob, T)
+                    # pyopencl.enqueue_copy(queue, reduced, balpha)
+                    # print 'reduced:\n', reduced
 
+                    e = pyopencl.enqueue_barrier(queue)
+                    e.wait()
                     start = t.time()
                     kernel.reduce(queue,
-                                (blocks*blocksize,), (blocksize,),
-                                bC, balpha, scratch, btransform, T)
-                    kernel.reduce(queue,
-                                (blocksize,), (blocksize,),
-                                balpha, balpha_T, scratch, btransform, numpy.int32(blocks))
+                            (blocks*blocksize,), (blocksize,),
+                            bC, balpha, scratch, btransform, numpy.int32(T))
+                    kernel.scan(queue,
+                            (blocksize,), (blocksize,),
+                            balpha, scratch, btransform, numpy.int32(blocks))
                     e = pyopencl.enqueue_barrier(queue)
                     e.wait()
                     end = t.time()
 
-                    C_T = numpy.zeros((N,N), numpy.float32)
-                    pyopencl.enqueue_copy(queue, C_T, balpha_T)
+                    C = numpy.zeros((T,N,N), numpy.float32)
+                    pyopencl.enqueue_copy(queue, C, bC)
+                    scanned = numpy.zeros((blocks,N,N), numpy.float32)
+                    pyopencl.enqueue_copy(queue, scanned, balpha)
+                    C_T = scanned[blocks-1]
+                    T0 = (T/BLOCK_UNITS) * BLOCK_UNITS
+                    for k in range(T%BLOCK_UNITS):
+                        C_T = C[T0+k].dot(C_T)
+                        C_T = C_T / numpy.sum(C_T)
+
                     alpha0 = pi * B[:, ob[0]]
                     alphaT = C_T.dot(alpha0)
                     alphaT = alphaT / numpy.sum(alphaT)
                     numpy.testing.assert_almost_equal(alphaT, alpha[T-1])
+                    # print 'scanned:\n', reduced
 
+                    if end-start <= minimum[2] and \
+                       blocks    >= minimum[1] and \
+                       blocksize >= minimum[0]:
+                       minimum = (blocksize, blocks, end-start)
+
+        print "minimum of {t} seconds achieved with block size {bs} and {b} groups".format(
+            b=minimum[1], bs=minimum[0], t=minimum[2])
+        print "speedup: {s}".format(s=c_time/(minimum[2]))
+
+
+    def test_forward_reduce(self):
+        A, B, pi = hmm.utility.get_models()['t2']
+        ob = numpy.loadtxt('data/hmm1.10000.dat', numpy.int16) # numpy.array([1,0,1,0,1], numpy.int16)
+        T = len(ob)
+        N,M = B.shape
+        kernel = self.get_kernel(N, M, T)
+        mf = pyopencl.mem_flags
+
+        # prepare buffers
+        bA = pyopencl.Buffer(context,
+            mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=A)
+        bB = pyopencl.Buffer(context,
+            mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=B)
+        bpi = pyopencl.Buffer(context,
+            mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=pi)
+        bob = pyopencl.Buffer(context,
+            mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=ob)
+        bC = pyopencl.Buffer(context, mf.READ_WRITE,
+            numpy.dtype('float32').itemsize * T*N*N)
+        balpha_T = pyopencl.Buffer(context, mf.WRITE_ONLY,
+            numpy.dtype('float32').itemsize * N*N)
+
+        # execute kernels
+        kernel.build_matrices(queue,
+                (64*256,), (256,),
+                bC, bA, bB, bpi, bob, numpy.int32(T))
+        e = pyopencl.enqueue_barrier(queue)
+        e.wait()
+
+        start = t.time()
+        _, alpha, _ = chmm.forward(A, B, pi, ob)
+        end = t.time()
+        c_time = end-start
+        print 'forward time in c: {t} seconds.'.format(t=c_time)
+
+        blockss    = [14*n for n in range(1,20)]
+        blocksizes = [2**n for n in range(9)]
+
+        minimum = (0, 0, 1.0)
+        print 'benchmarking hmm.lib.opencl.forward.reduce'
+        for blocksize in blocksizes:
+            for blocks in blockss:
+                if blocks <= 2*blocksize and blocks*blocksize*2 <= T:
+                    TOTAL_UNITS = 2*blocksize*blocks
+                    BLOCK_UNITS = 2*blocksize
+
+                    balpha = pyopencl.Buffer(context, mf.READ_WRITE,
+                        numpy.dtype('float32').itemsize * blocks*N*N)
+                    bdebug = pyopencl.Buffer(context, mf.WRITE_ONLY,
+                        numpy.dtype('float32').itemsize * TOTAL_UNITS*N*N)
+                    scratch = pyopencl.LocalMemory(
+                        numpy.dtype('float32').itemsize*BLOCK_UNITS*N*N)
+
+                    transform = numpy.array(
+                        calculate_index_table([0], numpy.log2(blocksize)+1),
+                        numpy.int32)
+                    btransform = pyopencl.Buffer(context,
+                        mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=transform)                    
+
+                    start = t.time()
+                    kernel.reduce(queue,
+                                (blocks*blocksize,), (blocksize,),
+                                bC, balpha, scratch, btransform, numpy.int32(T))
+
+                    e = pyopencl.enqueue_barrier(queue)
+                    e.wait()
+                    end = t.time()
+
+                    C = numpy.zeros((T,N,N), numpy.float32)
+                    pyopencl.enqueue_copy(queue, C, bC)
+
+                    reduced = numpy.zeros((blocks,N,N), numpy.float32)
+                    pyopencl.enqueue_copy(queue, reduced, balpha)
+                    C_T = reduced[0]
+                    for k in range(1, len(reduced)):
+                        C_T = reduced[k].dot(C_T)
+                        C_T = C_T / numpy.sum(C_T)
+
+                    T0 = (T/BLOCK_UNITS) * BLOCK_UNITS
+                    for k in range(T%BLOCK_UNITS):
+                        C_T = C[T0+k].dot(C_T)
+                        C_T = C_T / numpy.sum(C_T)
+
+                    alpha0 = pi * B[:, ob[0]]
+                    alphaT = C_T.dot(alpha0)
+                    alphaT = alphaT / numpy.sum(alphaT)
+                    numpy.testing.assert_almost_equal(alphaT, alpha[T-1])
+                    
                     if end-start <= minimum[2] and \
                        blocks    >= minimum[1] and \
                        blocksize >= minimum[0]:

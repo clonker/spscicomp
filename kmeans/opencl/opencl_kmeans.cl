6ef5fa9cdf64727c0f3a451bf462205cb296441d
Moritz Hoffmann <clonker@gmail.com>
2015-01-16 19:05:52 +0100

    Updated opencl implementation to use dimension and k as global program parameters.

--- kmeans/opencl/opencl_kmeans.cl
+++ kmeans/opencl/opencl_kmeans.cl
@@ -1,4 +1,5 @@
-
+#define DIM ${DIM}
+#define K ${K}
 
 kernel void kmeans_chunk_center_cl(
     global const float* data,
@@ -6,20 +7,18 @@ kernel void kmeans_chunk_center_cl(
     global const float* centers,
     global int* centersCounter,
     global float* newCenters,
-    global float* out,
-    const int dim,
-    const int n_centers
+    global float* out
 ) {
     unsigned int gid = get_global_id(0);
     unsigned int lid = get_local_id(0);
 
     int minCenter = 3;
     float minDist = INFINITY;
-    for(int c = 0; c < n_centers; c++) {
+    for(int c = 0; c < K; c++) {
         float currDist = 0;
-        for (int i = 0; i < dim; i++) {
-            float xi = data[dim*gid + i];
-            float ci = centers[dim*c + i];
+        for (int i = 0; i < DIM; i++) {
+            float xi = data[DIM*gid + i];
+            float ci = centers[DIM*c + i];
             currDist += (xi-ci)*(xi-ci);
         }
         currDist = sqrt(currDist);
@@ -37,9 +36,9 @@ kernel void kmeans_chunk_center_cl(
 
     if(lid == 0) {
         unsigned int offset = get_group_id(0) * get_local_size(0);
-        for(int k = 0; k < n_centers; k++) {
+        for(int k = 0; k < K; k++) {
             centersCounter[offset + k] = 0;
-            for(int d = 0; d < dim; d++) {
+            for(int d = 0; d < DIM; d++) {
                 newCenters[offset + k + d] = 0.0f;
             }
         }
@@ -48,7 +47,7 @@ kernel void kmeans_chunk_center_cl(
         for(unsigned int k = 0; k < get_local_size(0); k++) {
             int assign = assigns[offset+k];
             centersCounter[assign + offset] += 1;
-            for(int d = 0; d < dim; d++) {
+            for(int d = 0; d < DIM; d++) {
                 newCenters[offset + assign + d] += data[offset+k+d];
             }
         }
@@ -57,27 +56,28 @@ kernel void kmeans_chunk_center_cl(
     barrier(CLK_LOCAL_MEM_FENCE | CLK_GLOBAL_MEM_FENCE);
 
     if (gid == 0) {
-        for(int i = 0; i < n_centers; i++) {
+        for(int i = 0; i < K; i++) {
             centersCounter[i] += centersCounter[i + get_local_size(0)];
             for(uint g = 1; g < get_num_groups(0); g++) {
-                for(int d = 0; d < dim; d++) {
+                for(int d = 0; d < DIM; d++) {
                     newCenters[i+d] += newCenters[i + g + get_local_size(0)];
                 }
             }
         }
 
-        for(int k=0; k < n_centers; k++) {
+        for(int k=0; k < K; k++) {
             if(centersCounter[k] > 0) {
-                for(int d = 0; d < dim; d++) {
+                for(int d = 0; d < DIM; d++) {
                     newCenters[k+d] /= (float) centersCounter[k];
                 }
             } else {
-                for(int d = 0; d < dim; d++) {
+                for(int d = 0; d < DIM; d++) {
                     newCenters[k+d] = centers[k+d];
                 }
             }
         }
         out[2] = newCenters[0];
         out[3] = newCenters[1];
+        out[0] = 1234567;
     }
 }
--- kmeans/opencl/opencl_kmeans.py
+++ kmeans/opencl/opencl_kmeans.py
@@ -1,4 +1,5 @@
 import os
+import string
 
 import numpy as np
 import pyopencl as cl
@@ -14,27 +15,38 @@ LOG = logger.Logger(__name__).get()
 class OpenCLKmeans(DefaultKmeans):
     def __init__(self, metric=EuclideanMetric(), importer=None, chunk_size=1000, max_steps=100):
         super(OpenCLKmeans, self).__init__(metric, importer, chunk_size, max_steps)
-        platform = cl.get_platforms()
-        gpus = platform[0].get_devices(device_type=cl.device_type.CPU)
-        self.ctx = cl.Context(devices=gpus)
-        self.queue = cl.CommandQueue(self.ctx)
-        self.prg = OpenCLKmeans.load_cl_program(self.ctx, 'opencl_kmeans.cl')
-        self.n_work_groups = sum(gpu.max_compute_units for gpu in gpus)
+        self.kernel = None
+        self.ctx = None
+        self.queue = None
+        self.prg = None
+        self.n_work_groups = None
 
-    # @profile
+    def __initialize_program(self, dim, k):
+        platform = cl.get_platforms()
+        devices = platform[0].get_devices(device_type=cl.device_type.GPU)
+        self.ctx = cl.Context(devices)
+        self.queue = cl.CommandQueue(self.ctx)
+        self.prg = OpenCLKmeans.load_cl_program(self.ctx, 'opencl_kmeans.cl', dim, k)
+        self.n_work_groups = sum(device.max_compute_units for device in devices)
+
     def kmeans_chunk_center(self, data, centers):
         data = data.astype(np.float32)
         k = len(centers)
         dim = len(centers[0])
+
+        if not self.prg:
+            self.__initialize_program(dim, k)
+
         centers = np.asarray(centers, dtype=np.float32)
         out = np.zeros((len(data), 1), dtype=np.float32)
         centers_counter = np.asarray(k * [0] * self.n_work_groups, dtype=np.int32)
         new_centers = np.asarray(self.n_work_groups * [np.zeros(self._dimension) for _ in xrange(k)], dtype=np.float32)
         data_assigns = np.empty((len(data), 1), dtype=np.int32)
-        LOG.debug("data="+str(data))
-        LOG.debug("centers=" + str(centers))
-        LOG.debug("k=" + str(k) + ", dim=" + str(dim))
-        LOG.debug("n_work_groups=" + str(self.n_work_groups))
+
+        #LOG.debug("data=%s",str(data))
+        #LOG.debug("centers=" + str(centers))
+        #LOG.debug("k=" + str(k) + ", dim=" + str(dim))
+
         # create buffers
         data_buf = cl.Buffer(self.ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=data)
         centers_buf = cl.Buffer(self.ctx, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=centers)
@@ -55,29 +67,33 @@ class OpenCLKmeans(DefaultKmeans):
         #                                np.int32(k)
         #).wait()
 
-        self.prg.kmeans_chunk_center_cl(self.queue, (len(data),), None, data_buf, assigns_buf, centers_buf, centers_counter_buf, new_centers_buf, out_buf, np.int32(dim), np.int32(k)).wait()
+        self.prg.kmeans_chunk_center_cl(self.queue, (len(data),), None, data_buf, assigns_buf, centers_buf, centers_counter_buf, new_centers_buf, out_buf).wait()
 
         # wait for it to finish and read out buffers
-        #cl.enqueue_read_buffer(self.queue, assigns_buf, data_assigns).wait()
+        cl.enqueue_read_buffer(self.queue, assigns_buf, data_assigns).wait()
         cl.enqueue_read_buffer(self.queue, new_centers_buf, new_centers).wait()
         cl.enqueue_read_buffer(self.queue, centers_counter_buf, centers_counter).wait()
         cl.enqueue_read_buffer(self.queue, out_buf, out).wait()
 
+        LOG.debug("cc=" + str(centers_counter))
         centers_counter = centers_counter[:k]
         new_centers = new_centers[:k]
 
         self._data_assigns.append(data_assigns)
-        LOG.debug("cc=" + str(centers_counter))
         LOG.debug("new_centers=" + str(new_centers))
         LOG.debug("assigns=" + str(data_assigns))
-        LOG.debug("out=" + str(out))
+        #LOG.debug("out=" + str(out))
 
         return new_centers
 
     @staticmethod
-    def load_cl_program(context, program):
+    def load_cl_program(context, program, dimension, n_centers):
         with open(str(os.path.dirname(os.path.realpath(__file__))) + '/' + program, "r") as f:
             cl_program = f.read()
         if cl_program is not None:
+            cl_program = string.Template(cl_program).substitute(
+                DIM=dimension,
+                K=n_centers
+            )
             return cl.Program(context, cl_program).build()
         return None

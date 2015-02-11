446c265a05962653583976f43ea01058be803fdb
Yuchun-Zhang <yczhangvrc@gmail.com>
2015-02-11 14:26:59 +0100

    Modified setups, now it is able to compile c_extensions and CUDA files at the same time.

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from distutils.command.build_ext import build_ext
 import os
 from os.path import join as pjoin
 import numpy
-import distutils.ccompiler
 
 # adapted from https://github.com/rmcgibbo/npcuda-example
 def find_in_path(name, path):
